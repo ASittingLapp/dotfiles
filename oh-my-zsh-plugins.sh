11d6e31d43309fac162346d96ebdbccf1adfcc5e
Alex Lapp <watertower5132@gmail.com>
2019-09-25 18:57:52 -0500

    Add you-should-use to oh-my-zsh-plugins

--- oh-my-zsh-plugins.sh
+++ oh-my-zsh-plugins.sh
@@ -1,1 +1,2 @@
 git clone https://github.com/zdharma/fast-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ~/.oh-my-zsh/custom/plugins/you-should-use
